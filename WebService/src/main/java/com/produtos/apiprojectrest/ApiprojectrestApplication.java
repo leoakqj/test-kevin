3ac20084091a58e993e39a781accf52dc90cb5a9
Leonardo Camargo <lscifpr@gmail.com>
2022-03-15 23:00:13 -0300

    Inital commit

---
+++ WebService/src/main/java/com/produtos/apiprojectrest/ApiprojectrestApplication.java
@@ -0,0 +1,13 @@
+package com.produtos.apiprojectrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiprojectrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiprojectrestApplication.class, args);
+	}
+
+}
